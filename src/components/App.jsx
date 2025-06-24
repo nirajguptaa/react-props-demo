a40d2cd1b4afeab72db10844b8a32382b00e957a
Niraj Gupta <nirajgupta1292003@gmail.com>
2025-06-24 19:27:02 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Card from "./card"
 import Contacts from "../contacts"
 console.log(Contacts);
-
+function createCard(contact){
+  return (<Card 
+    key={contact.id}
+  name={contact.name}
+   imgURL={contact.imgURL}
+   phone={contact.phone}
+   email={contact.email}
+   />
+  );
+}
 function App() {
   return (
     <div>
       <h1 className="heading">Indian Cricketers</h1>
-      <Card
+      {Contacts.map(createCard)}
+      {/* <Card
         name={Contacts[0].name}
         imgURL={Contacts[0].imgURL}
         phone={Contacts[0].phone}
@@ -24,7 +34,7 @@ function App() {
         imgURL={Contacts[2].imgURL}
         phone={Contacts[2].phone}
         email={Contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
